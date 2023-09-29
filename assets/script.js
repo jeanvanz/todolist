ce5d1e3a8e25897b23f7a9a5ca9a5f9ee824e7d0
Jean Vanz <1134254@atitus.edu.br>
2023-09-28 21:34:56 -0300

    Adicionado o evento em js para ocultar ou mostrar a lista de tasks

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
-console.log("Funfeia")
+const tasks = []
+const taskListElement = document.getElementById("taskListElement");
+const emptyTaskListElement = document.getElementById("emptyTaskListElement");
+
+
+function newId(){
+    return Math.floor(Math.random()*1000);
+}
+
+function createTask(taskTitle, taskDescription = "") {
+    let id = newId();
+
+    let task = {
+        id,
+        taskTitle: taskTitle,
+        taskDescription: taskDescription,
+    }
+
+    tasks.push(task);
+    return task;
+}
+
+function renderTask(listElement = taskListElement, emptyMessage = emptyTaskListElement) {
+    let finalHtml = "";
+
+    if (tasks.length == 0) {
+        emptyMessage.style.display = 'block';
+        listElement.style.display = 'none';
+        return;
+    }
+
+    emptyMessage.style.display = 'none';
+    listElement.style.display = 'flex';
+}
+
+renderTask();   
